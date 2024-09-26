67abadde289fa69141286008f34435266faff292
guilhermefloriano1 <minervino.guilherme@escola.pr.gov.br>
2024-09-26 12:04:47 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+//Terror, suspense, drama, misterio
+
+//Terrifier, +18, terror
+//Panico na floresta, +14, terror, drama
+//It a coisa, +16, terror, misterio
+//Chucky, +16, terror, comedia
+//Sexta-feira 13, +18, terror, crime
+//A hora do pesadelo 2, +18, terror, fantasia
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
